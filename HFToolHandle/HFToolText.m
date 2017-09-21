6bd40d1f593eef91bec1fc95bb968a9d5d9a646e
qianhaifeng <qianhaifeng@quancheng-ec.com>
2017-09-21 13:30:55 +0800

    add

---
+++ HFToolHandle/HFToolText.m
@@ -0,0 +1,13 @@
+//
+//  HFToolText.m
+//  HFTool
+//
+//  Created by qhf on 2017/9/21.
+//  Copyright © 2017年 qhf. All rights reserved.
+//
+
+#import "HFToolText.h"
+
+@implementation HFToolText
+
+@end
